9cb8b146a332ee5877cc96d04f6e6e354be41ae2
CER-ArnauRP <a.rodriguez@ceroca.cat>
2024-03-06 19:43:42 +0100

    Login funcionant

--- lib/auth/servei_auth.dart
+++ lib/auth/servei_auth.dart
@@ -4,5 +4,24 @@ class ServeiAuth {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  
+  // Fer login
+  Future<UserCredential> loginAmbEmailIPassword(String email, password) async {
+
+    try {
+      UserCredential credencialUsuari = await _auth.signInWithEmailAndPassword(
+        email: email, 
+        password: password,
+      );
+
+      return credencialUsuari;
+      
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+    
+  }
+
+  // Fer registre
+
+  // Fer logout
 }
