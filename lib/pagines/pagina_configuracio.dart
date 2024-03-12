a588aaa5e0092cf5c6d7029d8465bf25996f365a
CER-ArnauRP <a.rodriguez@ceroca.cat>
2024-03-12 16:03:01 +0100

    Drawer i navegació entre pàgines

--- lib/pagines/pagina_configuracio.dart
+++ lib/pagines/pagina_configuracio.dart
@@ -1,26 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_2324/auth/servei_auth.dart';
 import 'package:flutter_firebase_2324/components/drawer_aplicacio.dart';
 
-class PaginaInici extends StatelessWidget {
-  const PaginaInici({super.key});
+class PaginaConfiguracio extends StatelessWidget {
+  const PaginaConfiguracio({super.key});
 
-  void logout() {
-    final serveiAuth = ServeiAuth();
-
-    serveiAuth.tancarSessio();
-  }
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Inici"),
+        title: const Text("Configuració"),
         backgroundColor: const Color.fromARGB(255, 255, 147, 108),
       ),
       drawer: const DrawerAplicacio(),
-      body: const Placeholder(
+      body: Container(
         color: Color.fromARGB(255, 250, 183, 159),
+        child: const Center(
+          child: Text("Configuració"),
+        ),
       ),
     );
   }
