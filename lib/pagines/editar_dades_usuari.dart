dca6ee390e071760d62d7085d5dab880b036abd7
CER-ArnauRP <a.rodriguez@ceroca.cat>
2024-04-10 20:24:53 +0200

    [No compila] PickerImatge App i Web

---
+++ lib/pagines/editar_dades_usuari.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class EditarDadesUsuari extends StatefulWidget {
+  const EditarDadesUsuari({super.key});
+
+  @override
+  State<EditarDadesUsuari> createState() => _EditarDadesUsuariState();
+}
+
+class _EditarDadesUsuariState extends State<EditarDadesUsuari> {
+
+  File? _imatgeSeleccionadaApp;
+  Uint8List? _imatgeSeleccionadaWeb;
+  bool _imatgeAPuntPerPujar = false;
+
+  Future<void> _triaImatge() async {
+
+    final ImagePicker picker = ImagePicker();
+    XFile? imatge = await picker.pickImage(source: ImageSource.gallery);
+
+    // Si trien i trobem la imatge.
+    if (imatge != null) {
+
+      // Si l'App s'executa en un dispositiu mòbil.
+      if (!kIsWeb) {
+
+        File arxiuSeleccionat = File(imatge.path);
+        
+        setState(() {
+          _imatgeSeleccionadaApp = arxiuSeleccionat;
+          _imatgeAPuntPerPujar = true;
+        });
+        
+      }
+
+      // Si l'App s'executa en un navegador web.
+      if (kIsWeb) {
+        Uint8List arxiuEnBytes = await imatge.readAsBytes();
+
+        setState(() {
+          _imatgeSeleccionadaWeb = arxiuEnBytes;
+          _imatgeAPuntPerPujar = true;
+        });
+      }
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Editar Dades"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+      
+              // Botó per obrir el FilePicker.
+              // =============================
+              GestureDetector(
+                onTap: _triaImatge(),
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                  ),
+                  child: const Text("Tria imatge"),
+                ),
+              ),
+
+              // Botó per pujar la imatge seleccionada amb FilePicker.
+              // =====================================================
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.blue[400],
+                  ),
+                  child: const Text("Puja imatge"),
+                ),
+              ),
+
+              // Visor del resultat del FilePicker.
+              // ==================================
+
+              // Visor del resultat de carregar la imatge de Firebase Storage.
+              // =============================================================
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
